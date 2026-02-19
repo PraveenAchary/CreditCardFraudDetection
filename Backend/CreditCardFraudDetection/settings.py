b1230c220d32a94b864fa2c298d7b9af094883b5
PraveenAchary <praveenvishwabramham@gmail.com>
2026-02-19 18:20:12 +0530

    Update settings.py

--- Backend/CreditCardFraudDetection/settings.py
+++ Backend/CreditCardFraudDetection/settings.py
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
